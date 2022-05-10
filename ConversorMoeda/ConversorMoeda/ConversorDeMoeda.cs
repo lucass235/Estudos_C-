0ae23c2f9a2f854110f011af9cc0ae277a5038dc
lucas235 <lucass235@gmail.com>
2022-05-10 14:54:57 -0300

    programas em C#

---
+++ ConversorMoeda/ConversorMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMoeda {
+    class ConversorDeMoeda {
+        public static double cotacaoDolar;
+        public static double dolar;
+
+        public static double valorPagoEmReais() {
+            
+            return (cotacaoDolar * dolar) * 1.06;
+        }
+    }
+}
---
+++ ConversorMoeda/ConversorMoeda/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConversorMoeda {
+    class Program {
+        static void Main(string[] args) {
+
+            double cotacao, dolar;
+
+            Console.Write("Qual é a contação do dólar: ");
+            ConversorDeMoeda.cotacaoDolar = double.Parse( Console.ReadLine());
+
+            Console.Write("Quantos dólares você vai comprar: ");
+            ConversorDeMoeda.dolar = double.Parse( Console.ReadLine());
+
+            Console.WriteLine("Valor a ser pago em reais: R$" +  ConversorDeMoeda.valorPagoEmReais().ToString("F2"));
+        }
+    }
+}
