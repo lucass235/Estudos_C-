d06f1b611eb917c94c67f9cc26ebd933b7e34e06
lucas235 <lucass235@gmail.com>
2022-05-26 10:38:08 -0300

    Projeto de monipulacao de arquivos

---
+++ PrecoDeProduto/PrecoDeProduto/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecoDeProduto {
+    class Produto {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int qtd { get; set; }
+
+        public Produto(string name, double price, int qtd) {
+            this.name = name;
+            this.price = price;
+            this.qtd = qtd;
+        }
+
+        public double total() {
+            return this.price * this.qtd;
+        }
+    }
+}
---
+++ PrecoDeProduto/PrecoDeProduto/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrecoDeProduto {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"C:\temp\priceProduct.csv"; // camimho de origem do arquivo
+            string targetPath = @"C:\temp\summary.csv"; // caminho de origem do arquivo
+            //File.Create(targetPath);
+            string[] lines = File.ReadAllLines(sourcePath); // guarda os dados de cada linha no array de string
+            string[] dados;
+            try {
+                using (StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        dados = line.Split(',');
+
+                        Produto prod = new Produto(dados[0],double.Parse(dados[1]), int.Parse(dados[2]));
+                        sw.WriteLine($"{prod.name}, R${prod.total()}");
+                    }
+                }
+                Console.WriteLine("Criado!");
+            }
+            catch (IOException e) {
+                Console.WriteLine("Erro de arquivo: " + e.Message);
+            }
+        }
+    }
+}
